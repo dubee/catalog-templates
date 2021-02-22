8e88f8d2eb7db05e4f12d8e2e3356808655aa59a
bsvihovec <bsvihovec@yahoo.com>
2021-02-22 16:59:32 -0500

    Create vars.tf

---
+++ offerings-manual/vars.tf
@@ -0,0 +1,23 @@
+variable "refresh_token" {
+}
+
+variable "token" {
+}
+
+variable "cluster_id" {
+}
+
+variable "region" {
+}
+
+variable "all_namespaces" {
+}
+
+variable "version_locator_id" {
+}
+
+variable "namespaces" {
+}
+
+variable "cm_global_host" {
+}
