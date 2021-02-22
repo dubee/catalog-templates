4e9550d01f130a7df59d07c68772cb51984f88a6
bsvihovec <bsvihovec@yahoo.com>
2021-02-22 16:54:36 -0500

    Create operator-install.tf

---
+++ offerings-manual/operator-install.tf
@@ -0,0 +1,11 @@
+resource "null_resource" "operator-deploy" {
+  provisioner "local-exec" {
+      command = <<EOT
+        curl -s -X POST https://cm.globalcatalog.test.cloud.ibm.com/api/v1-beta/deploy/kubernetes/olm/operator \
+             -H 'Content-Type: application/json' \
+             -H 'X-Auth-Refresh-Token: ${var.refresh_token}' \
+             -H 'Authorization: bearer ${var.token}' \
+             -d '{"cluster_id": "${var.cluster_id}", "region": "${var.region}", "all_namespaces": "${var.all_namespaces}", "version_locator_id": "${var.version_locator_id}", "namespaces": "${var.namespaces}"}'
+        EOT
+  } 
+}
