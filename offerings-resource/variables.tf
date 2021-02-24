30a9d73ff3c728c20a6a8fc359f693e053ed9b2f
bsvihovec <bsvihovec@yahoo.com>
2021-02-24 12:37:57 -0500

    Create variables.tf

---
+++ offerings-resource/variables.tf
@@ -0,0 +1,43 @@
+variable "iam_token" {
+    description= "iam bearer token for ibm cloud account"
+    type = string
+}
+variable "iam_refresh_token" {
+    description= "iam refresh token for ibm cloud account"
+    type = string
+}
+variable "instance_label" {
+    type = string
+}
+variable "catalog_id" {
+    description = "ID of the catalog which contains the offering you would like to deploy"
+    type = string
+}
+variable "offering_id" {
+    description = "ID of the offering you would like to deploy"
+    type = string
+}
+variable "kind_format" {
+    description = "kind of deployment of this offering"
+    type = string
+}
+variable "offering_version" {
+    description = "version of the offering you would like to deploy"
+    type = string
+}
+variable "cluster_id" {
+    description = "id of the cluster which you would like to deploy your offering into"
+    type = string
+}
+variable "region" {
+    description = "ibm cloud region"
+    type = string
+}
+variable "cluster_namespaces" {
+    description = "namespace in the cluster which you would like to deploy your offering into"
+    type = list
+}
+variable "cluster_all_namespaces" {
+    description = "boolean for whether or not you would like to deploy into all namespaces"
+    type = bool
+}
