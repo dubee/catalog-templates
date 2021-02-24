a8625cc0738547130be8beb4d46d551ffa02dcf7
bsvihovec <bsvihovec@yahoo.com>
2021-02-24 12:37:33 -0500

    Update main.tf

--- offerings-resource/main.tf
+++ offerings-resource/main.tf
@@ -1,1 +1,12 @@
 
+resource "ibm_cm_offering_instance" "cm_offering_instance_instance" {
+   label = var.instance_label 
+   catalog_id = var.catalog_id
+   offering_id = var.offering_id
+   kind_format = var.kind_format
+   version = var.offering_version
+   cluster_id = var.cluster_id
+   cluster_region = var.region
+   cluster_namespaces = var.cluster_namespaces
+   cluster_all_namespaces = var.cluster_all_namespaces
+}
