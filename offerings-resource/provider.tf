43a740f35803e933560b72a7a811b01cbb5e38b7
bsvihovec <bsvihovec@yahoo.com>
2021-02-25 13:28:02 -0500

    Update provider.tf

--- offerings-resource/provider.tf
+++ offerings-resource/provider.tf
@@ -1,3 +1,12 @@
 provider "ibm" {
   region           = var.region
 }
+terraform {
+  required_providers {
+    ibm = {
+	  # source is mandatory for community providers
+      source = "IBM-Cloud/ibm" 
+      version >= "1.13.1"
+    }
+  }
+}
