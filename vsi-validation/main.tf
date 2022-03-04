3a99276485a862b26290fd4077be7625acc2e981
dubeejw <jwdubee@us.ibm.com>
2022-03-04 15:42:23 -0500

    Update VSI template

--- vsi-validation/main.tf
+++ vsi-validation/main.tf
@@ -1,33 +1,20 @@
-resource "ibm_is_subnet" "validation" {
-  name            = "validation-subnet"
-  vpc             = var.vpc_id
-  zone            = var.subnet_zone
-  ipv4_cidr_block = var.subnet_ipv4_cidr_block
-  ip_version      = "ipv4"
-}
-
-resource "ibm_is_ssh_key" "validation" {
-  name       = "validation-ssh"
-  public_key = var.ssh_public_key
-}
-
-resource "ibm_is_instance" "validation" {
-  name    = "validation-instance"
+resource "ibm_is_instance" "validation_instance" {
+  name    = var.vsi_instance_name
   image   = var.vsi_id
   profile = var.vpc_profile
 
   primary_network_interface {
-    subnet = ibm_is_subnet.validation.id
+    subnet = var.subnet_id
   }
 
   network_interfaces {
     name   = "eth1"
-    subnet = ibm_is_subnet.validation.id
+    subnet = var.subnet_id
   }
 
   vpc  = var.vpc_id
   zone = var.subnet_zone
-  keys = [ibm_is_ssh_key.validation.id]
+  keys = [var.ssh_key_id]
 
   timeouts {
     create = "15m"
--- vsi-validation/variables.tf
+++ vsi-validation/variables.tf
@@ -1,3 +1,23 @@
+variable "vsi_instance_name" {
+    description = "Name of the VSI instance to create"
+    type = string
+}
+
+variable "vsi_id" {
+    description = "ID of VSI to deploy"
+    type = string
+}
+
+variable "vpc_profile" {
+    description = "Profile of VPC"
+    type = string
+}
+
+variable "subnet_id" {
+    description = "ID of subnet to provision with"
+    type = string
+}
+
 variable "vpc_id" {
     description = "ID of the VPC to deploy to"
     type = string
@@ -8,22 +28,7 @@ variable "subnet_zone" {
     type = string
 }
 
-variable "vsi_id" {
-    description = "ID of VSI to deploy"
-    type = string
-}
-
-variable "ssh_public_key" {
-    description = "SSH public key"
-    type = string
-}
-
-variable "vpc_profile" {
-    description = "Profile of VPC"
-    type = string
-}
-
-variable "subnet_ipv4_cidr_block" {
-    description = "IPv4 CIDR block"
+variable "ssh_key_id" {
+    description = "ID of SSH key to provision with"
     type = string
 }
