45066040b541756f456681fa558cb8d71573e4e0
dubee <jwdubee@us.ibm.com>
2025-04-29 12:58:14 -0400

    Remove extra network interface

--- vsi-validation/main.tf
+++ vsi-validation/main.tf
@@ -12,11 +12,6 @@ resource "ibm_is_instance" "validation_instance" {
     subnet = var.subnet_id
   }
 
-  network_interfaces {
-    name   = "eth1"
-    subnet = var.subnet_id
-  }
-
   vpc  = var.vpc_id
   zone = var.subnet_zone
   keys = [var.ssh_key_id]
